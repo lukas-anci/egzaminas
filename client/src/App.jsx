25831fc3acd37c9eb5ad2a066a419ac5bf6bed45
Lukas <lukas.ancikevicius@gmail.com>
2021-07-28 09:38:18 +0300

    react clean up, npm i axios,bootstrap

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import React, { Component } from 'react';
+class App extends Component {
+  state = {};
+  render() {
+    return <div className="App">hello</div>;
+  }
+}
+
+export default App;
