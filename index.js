bcff10d795c954184864da226dd79569ea0dfdba
Lukas <lukas.ancikevicius@gmail.com>
2021-07-28 09:18:55 +0300

    npm i cors,express,denv,morgan mongoose, cra

---
+++ index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 5000;
