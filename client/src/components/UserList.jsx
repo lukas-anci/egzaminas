ce74ecbd365132b639af77611f121b9977af8a45
Lukas <lukas.ancikevicius@gmail.com>
2021-07-28 12:06:55 +0300

    changed to lithuanian no user title

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -4,7 +4,7 @@ class UserList extends Component {
   state = {};
   render() {
     if (!this.props.users.length)
-      return <h2>No users yet, please create one </h2>;
+      return <h2>Nėra vartotojų, prašome sukurti vieną </h2>;
     return (
       <div className="d-flex flex-wrap">
         {this.props.users.map((u) => (
