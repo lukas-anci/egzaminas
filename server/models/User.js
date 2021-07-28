24d2c9734c70380570ef590769cfc8a391779118
Lukas <lukas.ancikevicius@gmail.com>
2021-07-28 09:25:08 +0300

    userModel  created

---
+++ server/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model('userscrud', userSchema);
+
+module.exports = UserModel;
