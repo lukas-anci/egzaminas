5a8ed5bbdce523f832803a87cf126682d7fc9077
Lukas <lukas.ancikevicius@gmail.com>
2021-07-28 09:20:28 +0300

    back end running, connected to mongoose

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// MiddleWare
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+// connect to mongoose
+mongoose
+  .connect(process.env.MONGO_CONNECT_STRING, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log('connected to mongoose server');
+  })
+  .catch((err) => console.error(err.message));
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
